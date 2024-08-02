64849366e477500866b2086be7c937357e4a2c9c
vagnersantosbsi <47819614+vagnersantosbsi@users.noreply.github.com>
2024-08-02 11:44:50 -0300

    Add files via upload

---
+++ src/components/MenuPrincipal.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Criaremos os links para 
+     as páginas componentes -->
+    <!-- Usando <router-link></router-link> -->
+</template>
+
+
+<script>
+    // Colocaremos os scripts dos componentes
+    // utilizados na rota
+
+</script>
+
+<style>
+    /* Estilização dos componentes */
+    
+</style>
